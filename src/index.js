4e037f9680b88c43ca744fdf6b750531770a7e78
Vladislav Zolotarev <vladzolotarev2610@gmail.com>
2020-03-24 11:21:46 +0200

    setup firebase

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBv-oyhYjCqzoQpOmxvJoIRl_TpgG0vdHE",
+    authDomain: "music-playlist-53e8c.firebaseapp.com",
+    databaseURL: "https://music-playlist-53e8c.firebaseio.com",
+    projectId: "music-playlist-53e8c",
+    storageBucket: "music-playlist-53e8c.appspot.com",
+    messagingSenderId: "653290300461",
+    appId: "1:653290300461:web:311e4a495ac4d0f756c4a4"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
